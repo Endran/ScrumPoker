6d818ac2545ab03a8a1023f63f338c1e4a40b51e
David Hardy <david.hardy@philips.com>
2015-11-07 22:34:47 +0100

    Finalised drawer menu

--- app/src/main/java/nl/endran/scrumpoker/MainActivity.java
+++ app/src/main/java/nl/endran/scrumpoker/MainActivity.java
@@ -60,13 +60,13 @@ public class MainActivity extends BaseActivity {
             }
         });
 
-        setCardsAndShow(CardValue.getStandard());
-
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
+        setCardsAndShow(CardValue.getStandard());
     }
 
     private void setCardsAndShow(final CardValue[] cardValues) {
